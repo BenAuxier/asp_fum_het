79b9cae29dde598be1b069156381db39e11fe7c6
BenAuxier <benauxier@gmail.com>
2022-11-18 06:14:25 +0100

    Add files via upload

---
+++ related_genomes_downloader.sh
@@ -0,0 +1,148 @@
+#udagawae genomes
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/078/395/GCA_001078395.1_Aud_assembly01/GCA_001078395.1_Aud_assembly01_genomic.fna.gz
+#	gunzip GCA_001078395.1_Aud_assembly01_genomic.fna.gz
+#	mv GCA_001078395.1_Aud_assembly01_genomic.fna Auda_IFM_46973.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/723/835/GCA_010723835.1_ASM1072383v1/GCA_010723835.1_ASM1072383v1_genomic.fna.gz
+#	gunzip GCA_010723835.1_ASM1072383v1_genomic.fna.gz
+#	mv GCA_010723835.1_ASM1072383v1_genomic.fna Auda_IFM_46972.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/255/GCA_010724255.1_ASM1072425v1/GCA_010724255.1_ASM1072425v1_genomic.fna.gz
+#	gunzip GCA_010724255.1_ASM1072425v1_genomic.fna.gz
+#	mv GCA_010724255.1_ASM1072425v1_genomic.fna Auda_IFM_51744.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/335/GCA_010724335.1_ASM1072433v1/GCA_010724335.1_ASM1072433v1_genomic.fna.gz
+#	gunzip GCA_010724335.1_ASM1072433v1_genomic.fna.gz
+#	mv GCA_010724335.1_ASM1072433v1_genomic.fna Auda_IFM_53868.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/135/GCA_010724135.1_ASM1072413v1/GCA_010724135.1_ASM1072413v1_genomic.fna.gz
+#	gunzip GCA_010724135.1_ASM1072413v1_genomic.fna.gz
+#	mv GCA_010724135.1_ASM1072413v1_genomic.fna Auda_IFM_5058.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/795/GCA_010724795.1_ASM1072479v1/GCA_010724795.1_ASM1072479v1_genomic.fna.gz
+#	gunzip GCA_010724795.1_ASM1072479v1_genomic.fna.gz
+#	mv GCA_010724795.1_ASM1072479v1_genomic.fna Auda_IFM_61606.fna
+
+#fisheri genomes
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/149/645/GCA_000149645.3_ASM14964v3/GCA_000149645.3_ASM14964v3_genomic.fna.gz
+#	gunzip GCA_000149645.3_ASM14964v3_genomic.fna.gz 
+#	mv GCA_000149645.3_ASM14964v3_genomic.fna Afis_NRRL_181.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/014/250/575/GCA_014250575.1_ASM1425057v1/GCA_014250575.1_ASM1425057v1_genomic.fna.gz
+#	gunzip GCA_014250575.1_ASM1425057v1_genomic.fna.gz
+#	mv GCA_014250575.1_ASM1425057v1_genomic.fna Afis_NRRL_4585.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/008/711/225/GCA_008711225.1_ASM871122v1/GCA_008711225.1_ASM871122v1_genomic.fna.gz
+#	gunzip GCA_008711225.1_ASM871122v1_genomic.fna.gz
+#	mv GCA_008711225.1_ASM871122v1_genomic.fna Afis_IBT_3003.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/008/711/165/GCA_008711165.1_ASM871116v1/GCA_008711165.1_ASM871116v1_genomic.fna.gz
+#	gunzip GCA_008711165.1_ASM871116v1_genomic.fna.gz
+#	mv GCA_008711165.1_ASM871116v1_genomic.fna Afis_IBT_3007.fna
+#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/016/748/045/GCA_016748045.1_ASM1674804v1/GCA_016748045.1_ASM1674804v1_genomic.fna.gz
+
+
+#lentulus genomes
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/723/975/GCA_010723975.1_ASM1072397v1/GCA_010723975.1_ASM1072397v1_genomic.fna.gz
+#	gunzip GCA_010723975.1_ASM1072397v1_genomic.fna.gz
+#	mv GCA_010723975.1_ASM1072397v1_genomic.fna Alen_IFM_47457.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/915/GCA_010724915.1_ASM1072491v1/GCA_010724915.1_ASM1072491v1_genomic.fna.gz
+#	gunzip GCA_010724915.1_ASM1072491v1_genomic.fna.gz
+#	mv GCA_010724915.1_ASM1072491v1_genomic.fna Alen_IFM_62316.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/445/GCA_012656445.1_ASM1265644v1/GCA_012656445.1_ASM1265644v1_genomic.fna.gz
+#	gunzip GCA_012656445.1_ASM1265644v1_genomic.fna.gz
+#	mv GCA_012656445.1_ASM1265644v1_genomic.fna Alen_CNM-CM7927.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/435/GCA_012656435.1_ASM1265643v1/GCA_012656435.1_ASM1265643v1_genomic.fna.gz
+#	gunzip GCA_012656435.1_ASM1265643v1_genomic.fna.gz
+#	mv GCA_012656435.1_ASM1265643v1_genomic.fna Alen_CNM-CM6069.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/335/GCA_012656335.1_ASM1265633v1/GCA_012656335.1_ASM1265633v1_genomic.fna.gz
+#	gunzip GCA_012656335.1_ASM1265633v1_genomic.fna
+#	mv GCA_012656335.1_ASM1265633v1_genomic.fna Alen_CNM-CM8927.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/375/GCA_012656375.1_ASM1265637v1/GCA_012656375.1_ASM1265637v1_genomic.fna.gz
+#	gunzip GCA_012656375.1_ASM1265637v1_genomic.fna.gz
+#	mv GCA_012656375.1_ASM1265637v1_genomic.fna Alen_CNM-CM8694.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/395/GCA_012656395.1_ASM1265639v1/GCA_012656395.1_ASM1265639v1_genomic.fna.gz
+#	gunzip GCA_012656395.1_ASM1265639v1_genomic.fna.gz
+#	mv GCA_012656395.1_ASM1265639v1_genomic.fna Alen_CNM-CM8060.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/455/GCA_012656455.1_ASM1265645v1/GCA_012656455.1_ASM1265645v1_genomic.fna.gz
+#	gunzip GCA_012656455.1_ASM1265645v1_genomic.fna.gz
+#	mv GCA_012656455.1_ASM1265645v1_genomic.fna Alen_CNM-CM6936.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/455/GCA_010724455.1_ASM1072445v1/GCA_010724455.1_ASM1072445v1_genomic.fna.gz
+#	gunzip GCA_010724455.1_ASM1072445v1_genomic.fna.gz
+#	mv GCA_010724455.1_ASM1072445v1_genomic.fna Alen_IFM_58399.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/445/615/GCA_001445615.1_Alt_assembly01/GCA_001445615.1_Alt_assembly01_genomic.fna.gz
+#	gunzip GCA_001445615.1_Alt_assembly01_genomic.fna.gz
+#	mv GCA_001445615.1_Alt_assembly01_genomic.fna Alen_IFM_54703.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/575/GCA_010724575.1_ASM1072457v1/GCA_010724575.1_ASM1072457v1_genomic.fna.gz
+#	gunzip GCA_010724575.1_ASM1072457v1_genomic.fna.gz
+#	mv GCA_010724575.1_ASM1072457v1_genomic.fna Alen_IFM_60648.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/010/724/635/GCA_010724635.1_ASM1072463v1/GCA_010724635.1_ASM1072463v1_genomic.fna.gz
+#	gunzip GCA_010724635.1_ASM1072463v1_genomic.fna.gz
+#	mv GCA_010724635.1_ASM1072463v1_genomic.fna Alen_IFM_61392.fna
+
+#skipping since there is only a single novofumigatus
+##novofumigatus genomes
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/847/465/GCF_002847465.1_Aspnov1/GCF_002847465.1_Aspnov1_genomic.fna.gz#
+#	gunzip GCF_002847465.1_Aspnov1_genomic.fna.gz#
+#	mv GCF_002847465.1_Aspnov1_genomic.fna Anov_IBT_16806.fna#
+
+#fumigatus genomes
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/005/768/625/GCA_005768625.2_ASM576862v2/GCA_005768625.2_ASM576862v2_genomic.fna.gz
+#	gunzip GCA_005768625.2_ASM576862v2_genomic.fna.gz
+#	mv GCA_005768625.2_ASM576862v2_genomic.fna Afum_SGAir0713.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/003/069/565/GCA_003069565.1_ASM306956v1/GCA_003069565.1_ASM306956v1_genomic.fna.gz
+#	gunzip GCA_003069565.1_ASM306956v1_genomic.fna.gz
+#	mv GCA_003069565.1_ASM306956v1_genomic.fna Afum_SPS-2.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/715/275/GCA_001715275.2_ASM171527v2/GCA_001715275.2_ASM171527v2_genomic.fna.gz
+#	gunzip GCA_001715275.2_ASM171527v2_genomic.fna.gz
+#	mv GCA_001715275.2_ASM171527v2_genomic.fna Afum_LMB-35Aa.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/643/665/GCA_001643665.1_ASM164366v1/GCA_001643665.1_ASM164366v1_genomic.fna.gz
+#	gunzip GCA_001643665.1_ASM164366v1_genomic.fna.gz
+#	mv GCA_001643665.1_ASM164366v1_genomic.fna Afum_ISSFT-21.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/643/655/GCA_001643655.1_ASM164365v1/GCA_001643655.1_ASM164365v1_genomic.fna.gz
+#	gunzip GCA_001643655.1_ASM164365v1_genomic.fna.gz
+#	mv GCA_001643655.1_ASM164365v1_genomic.fna Afum_IF1SW-F4.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/125/GCA_012656125.1_ASM1265612v1/GCA_012656125.1_ASM1265612v1_genomic.fna.gz
+#	gunzip GCA_012656125.1_ASM1265612v1_genomic.fna.gz
+#	mv GCA_012656125.1_ASM1265612v1_genomic.fna Afum_DNM-CM8689.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/234/955/GCA_002234955.1_ASM223495v1/GCA_002234955.1_ASM223495v1_genomic.fna.gz
+#	gunzip GCA_002234955.1_ASM223495v1_genomic.fna.gz
+#	mv GCA_002234955.1_ASM223495v1_genomic.fna Afum_HMR_AF_270.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/115/GCA_012656115.1_ASM1265611v1/GCA_012656115.1_ASM1265611v1_genomic.fna.gz
+#	gunzip GCA_012656115.1_ASM1265611v1_genomic.fna.gz
+#	mv GCA_012656115.1_ASM1265611v1_genomic.fna Afum_CNM-CM8686.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/234/985/GCA_002234985.1_ASM223498v1/GCA_002234985.1_ASM223498v1_genomic.fna.gz
+#	gunzip GCA_002234985.1_ASM223498v1_genomic.fna.gz
+#	mv GCA_002234985.1_ASM223498v1_genomic.fna Afum_HMR_AF_706.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/215/GCA_012656215.1_ASM1265621v1/GCA_012656215.1_ASM1265621v1_genomic.fna.gz
+#	gunzip GCA_012656215.1_ASM1265621v1_genomic.fna.gz
+#	mv GCA_012656215.1_ASM1265621v1_genomic.fna Afum_CNM-CM8057.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/165/GCA_012656165.1_ASM1265616v1/GCA_012656165.1_ASM1265616v1_genomic.fna.gz
+#	gunzip GCA_012656165.1_ASM1265616v1_genomic.fna.gz
+#	mv GCA_012656165.1_ASM1265616v1_genomic.fna Afum_CNM-CM8812.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/012/656/185/GCA_012656185.1_ASM1265618v1/GCA_012656185.1_ASM1265618v1_genomic.fna.gz
+#	gunzip GCA_012656185.1_ASM1265618v1_genomic.fna.gz
+#	mv GCA_012656185.1_ASM1265618v1_genomic.fna Afum_CNM-CM8714.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/150/145/GCA_000150145.1_ASM15014v1/GCA_000150145.1_ASM15014v1_genomic.fna.gz
+#	gunzip GCA_000150145.1_ASM15014v1_genomic.fna.gz
+#	mv GCA_000150145.1_ASM15014v1_genomic.fna Afum_A1163.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/225/645/GCA_000225645.2_CA_ASPER002/GCA_000225645.2_CA_ASPER002_genomic.fna.gz
+#	gunzip GCA_000225645.2_CA_ASPER002_genomic.fna.gz
+#	mv GCA_000225645.2_CA_ASPER002_genomic.fna Afum_AF210.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/225/625/GCA_000225625.2_ASPER001/GCA_000225625.2_ASPER001_genomic.fna.gz
+#	gunzip GCA_000225625.2_ASPER001_genomic.fna.gz
+#	mv GCA_000225625.2_ASPER001_genomic.fna Afum_Af10.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/002/655/GCA_000002655.1_ASM265v1/GCA_000002655.1_ASM265v1_genomic.fna.gz
+#	gunzip GCA_000002655.1_ASM265v1_genomic.fna.gz
+#	mv GCA_000002655.1_ASM265v1_genomic.fna Afum_Af293.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/029/325/GCA_001029325.1_ASM102932v1/GCA_001029325.1_ASM102932v1_genomic.fna.gz
+#	gunzip GCA_001029325.1_ASM102932v1_genomic.fna.gz
+#	mv GCA_001029325.1_ASM102932v1_genomic.fna Afum_Z5.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/731/615/GCA_000731615.1_AspFumNiv1.0/GCA_000731615.1_AspFumNiv1.0_genomic.fna.gz
+#	gunzip GCA_000731615.1_AspFumNiv1.0_genomic.fna.gz
+#	mv GCA_000731615.1_AspFumNiv1.0_genomic.fna Afum_niveus_RP-2014.fna
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/599/455/GCA_001599455.1_JCM_10253_assembly_v001/GCA_001599455.1_JCM_10253_assembly_v001_genomic.fna.gz
+#	gunzip GCA_001599455.1_JCM_10253_assembly_v001_genomic.fna.gz
+#	mv GCA_001599455.1_JCM_10253_assembly_v001_genomic.fna Afum_JCM_10253.fna
+#then have our sequenced trains Afum_p20.fna
+#				Afum_p21.fna
+
+#for i in *.fna; do makeblastdb ;done
+
+#for i in *.fna; do samtools faidx $i; done
+
+for i in *.fna; do augustus --species=aspergillus_fumigatus --gff3=on $i > ${i/\.fna/\.gff3}; done
+
+for i in *.fna; do gffread ${i/\.fna/\.gff3} -g $i -y ${i/\.fna/\.faa}; done
